9fd23e08d047d7e0c860bb907363e80c61f8357f
Nishtha Pardesi <nishthapardesi2005@gmail.com>
2025-09-29 20:08:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-// Set view engine to EJS
 app.set("view engine", "ejs");
 
-// Serve static files like CSS, images, etc. from "public" folder
 app.use(express.static("public"));
 
-// Root route
 app.get("/", (req, res) => {
   res.render("index");
 });
